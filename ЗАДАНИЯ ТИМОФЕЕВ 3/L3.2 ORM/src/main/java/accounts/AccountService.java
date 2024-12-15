38f280b7f269ad5198c2ead585a61713724c1643
JASPERMOGLOT <timofeevivan2004@gmail.com>
2024-12-15 23:11:21 +0500

    Tasks

---
+++ ЗАДАНИЯ ТИМОФЕЕВ 3/L3.2 ORM/src/main/java/accounts/AccountService.java
@@ -0,0 +1,21 @@
+package accounts;
+
+import dbService.DBException;
+import dbService.DBService;
+import dbService.dataSets.UsersDataSet;
+
+public class AccountService {
+
+    private DBService dbService = new DBService();
+
+
+   public void addNewUser(UserProfile userProfile) throws DBException {
+        dbService.addUser(userProfile.getLogin(), userProfile.getPassword());
+    }
+
+    public UserProfile getUserByLogin(String login) {
+        UsersDataSet usersDataSet = dbService.getUserByName(login);
+        return new UserProfile(usersDataSet.getName(), usersDataSet.getPassword());
+    }
+
+}
